80be588ce766ae3bcd4662b0b47f08f4cd017e37
Jianing Zhang <jzhang718@gmail.com>
2022-05-12 21:45:11 -0400

    final fixes

--- server/api/google-login.js
+++ server/api/google-login.js
@@ -1,47 +1,48 @@
 const router = require("express").Router();
 const {
-  models: { User },
+  models: { User, Order },
 } = require("../db");
 
-const dotenv = require('dotenv');
-const {OAuth2Client} = require('google-auth-library');
-dotenv.config()
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
-const googlePW = process.env.GOOGLE_PW
+const dotenv = require("dotenv");
+const { OAuth2Client } = require("google-auth-library");
+dotenv.config();
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const googlePW = process.env.GOOGLE_PW;
 
-router.post('/', async(req,res) => {
+router.post("/", async (req, res) => {
   const token = req.body.token.xc.id_token;
 
   const ticket = await client.verifyIdToken({
-    idToken : token,
-    audience: process.env.GOOGLE_CLIENT_ID
-  })
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
 
-  const { given_name, family_name, email} = ticket.getPayload();
+  const { given_name, family_name, email } = ticket.getPayload();
 
-  const user = await User.findOne({where: {
-    googleId: req.body.token.googleId
-  }})
+  const user = await User.findOne({
+    where: {
+      googleId: req.body.token.googleId,
+    },
+  });
 
-  if(!user) {
-    const newUser= await User.create({googleId: req.body.token.googleId, username: given_name, email:email, password: googlePW})
-    res.json(newUser)
-  }
-  else res.send(user)
-  
- 
- 
-})
-
+  if (!user) {
+    const newUser = await User.create({
+      googleId: req.body.token.googleId,
+      username: given_name,
+      email: email,
+      password: googlePW,
+    });
+    await Order.create({ isCart: true, userId: newUser.id });
+    res.json(newUser);
+  } else res.send(user);
+});
 
 router.get("/", (req, res, next) => {
   try {
-    res.send('hello world')
+    res.send("hello world");
   } catch (err) {
     next(err);
   }
 });
 
 module.exports = router;
-
-
